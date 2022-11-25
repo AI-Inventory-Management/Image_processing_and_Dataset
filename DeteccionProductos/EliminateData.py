78fe1a66ba0c7d2a90a92f1d9fedd5a97fbfe059
AlDomL9 <A01378028@itesm.mx>
2022-11-24 18:00:25 -0600

    Data deletion corr

--- DeteccionProductos/EliminateData.py
+++ DeteccionProductos/EliminateData.py
@@ -1,3 +1,6 @@
 import os
-os.remove("./data/store_data.json")
-os.remove("./data/fridge_data.json")
+try:
+    os.remove("./data/store_data.json")
+    os.remove("./data/fridge_data.json")
+except:
+    print("No files to delete")
